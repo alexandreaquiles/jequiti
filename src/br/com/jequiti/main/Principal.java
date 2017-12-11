c41c07bf671fd08c0a69740d9a14348b79af7bea
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-12-11 12:36:43 -0200

    Usando classes anônimas

--- src/br/com/jequiti/main/Principal.java
+++ src/br/com/jequiti/main/Principal.java
@@ -4,15 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import br.com.jequiti.dominio.Produto;
+import br.com.jequiti.dominio.filtro.ComparadorProdutos;
 import br.com.jequiti.dominio.filtro.FiltroDeProdutos;
-import br.com.jequiti.dominio.filtro.comparadores.ComparadorParaAmigoSecreto;
-import br.com.jequiti.dominio.filtro.comparadores.ComparadorParaColonias;
-import br.com.jequiti.dominio.filtro.comparadores.ComparadorParaProdutosMasculinos;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		Produto p1 = new Produto("COLONIA DESODORANTE MASCULINA FÁBIO JR.", 16.9);
 		Produto p2 = new Produto("ESMALTE JEQUITI AVIVA BARILOCHE", 6.9);
 		Produto p3 = new Produto("ESMALTE JEQUITI AVIVA TÓQUIO", 6.9);
@@ -23,13 +21,17 @@ public class Principal {
 		Produto p8 = new Produto("PALETTE DE SOMBRAS JEQUITI AVIVA 6 CORES", 59.9);
 		Produto p9 = new Produto("COLONIA DESODORANTE FEMININA ISABELLA FIORENTINO BELLA", 19.9);
 		Produto p10 = new Produto("ESTOJO ELIANA CRISTAL", 129.9);
-		
-		List<Produto> produtos = Arrays.asList(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10);
-		
+
+		List<Produto> produtos = Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10);
+
 		FiltroDeProdutos filtro = new FiltroDeProdutos();
-		
+
 		// Presentes para amigo secreto (mais de R$ 50)
-		List<Produto> amigoSecreto = filtro.filtra(produtos, new ComparadorParaAmigoSecreto());
+		List<Produto> amigoSecreto = filtro.filtra(produtos, new ComparadorProdutos() {
+			public boolean compara(Produto p) {
+				return p.getPreco() > 50;
+			}
+		});
 		for (Produto p : amigoSecreto) {
 			System.out.println(p);
 		}
@@ -37,7 +39,11 @@ public class Principal {
 		System.out.println("-----------------------------------------------------------------");
 
 		// Colônias
-		List<Produto> colonias = filtro.filtra(produtos, new ComparadorParaColonias());
+		List<Produto> colonias = filtro.filtra(produtos, new ComparadorProdutos() {
+			public boolean compara(Produto p) {
+				return p.getNome().startsWith("COLONIA");
+			}
+		});
 		for (Produto p : colonias) {
 			System.out.println(p);
 		}
@@ -45,7 +51,11 @@ public class Principal {
 		System.out.println("-----------------------------------------------------------------");
 
 		// Produtos Masculinos
-		List<Produto> masculinos = filtro.filtra(produtos, new ComparadorParaProdutosMasculinos());
+		List<Produto> masculinos = filtro.filtra(produtos, new ComparadorProdutos() {
+			public boolean compara(Produto p) {
+				return p.getNome().contains("MASCULIN");
+			}
+		});
 		for (Produto p : masculinos) {
 			System.out.println(p);
 		}
